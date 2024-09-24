e408512ec95b9fc1a9ae8305f40b19e5a0b3adec
ibtisam adil <ibtisamadil273@gmail.com>
2024-09-24 14:28:30 +0200

    Spawning and hiding project completed

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 // Name: IBTISAM ADIL
 
+struct Circle;
 
 constexpr int CIRCLE_RADIUS = 16;
+int CIRCLE_SPAWNED{};
+vector <Circle> circles;
 
 struct Position {
 	float x;
@@ -14,6 +17,9 @@ struct Position {
 
 struct Circle {
 	Position position;
+	Color circle_color;
+
+	Circle() : circle_color(white) {}
 
 	Circle create_circle() {
 		Circle new_circle;
@@ -21,16 +27,21 @@ struct Circle {
 		new_circle.position.y = static_cast<float>(get_mouse_y());
 		return new_circle;
 	}
-};
 
-Color random_color() {
-	Color color;
-	color.r = static_cast<unsigned char>(random_int_in_range(0, 255));
-	color.g = static_cast<unsigned char>(random_int_in_range(0, 255));
-	color.b = static_cast<unsigned char>(random_int_in_range(0, 255));
-	color.a = static_cast<unsigned char>(random_int_in_range(0, 255));
-	return color;
-}
+	Color random_color() {
+		Color color;
+		color.r = static_cast<unsigned char>(random_int_in_range(0, 255));
+		color.g = static_cast<unsigned char>(random_int_in_range(0, 255));
+		color.b = static_cast<unsigned char>(random_int_in_range(0, 255));
+		color.a = static_cast<unsigned char>(random_int_in_range(0, 255));
+		return color;
+	}
+
+	void change_color() {
+		circle_color = random_color();
+	}
+
+};
 
 bool is_mouse_over_circle(Position position_of_mouse, Circle circle) {
 	float distane_x = position_of_mouse.x - circle.position.x;
@@ -38,8 +49,6 @@ bool is_mouse_over_circle(Position position_of_mouse, Circle circle) {
 
 	return (distane_x * distane_x + distance_y * distance_y) <= (CIRCLE_RADIUS * CIRCLE_RADIUS);
 }
-
-vector <Circle> circles;
 
 void update() {
 	Position position_of_mouse;
@@ -49,20 +58,25 @@ void update() {
 	for (auto it = circles.begin(); it != circles.end(); ++it) {
 		if (is_mouse_over_circle(position_of_mouse, *it) && is_button_pressed(Button::Right)) {
 			circles.erase(it);
+			CIRCLE_SPAWNED--;
 			return;
+		}
+		if (is_mouse_over_circle(position_of_mouse, *it) && is_button_pressed(Button::Left)) {
+			it->circle_color = it->random_color();
 		}
 	}
 
-	if (is_button_pressed(Button::Right)) {
+	if (is_button_pressed(Button::Right) && CIRCLE_SPAWNED < 12) {
 		Circle new_circle = Circle().create_circle();
 		circles.push_back(new_circle);
+		CIRCLE_SPAWNED++;
 	}
 
 }
 
 void render() {
 	for (const Circle& circle : circles) {
-		draw_circle(round_to_int(circle.position.x), round_to_int(circle.position.y), CIRCLE_RADIUS);
+		draw_circle(round_to_int(circle.position.x), round_to_int(circle.position.y), CIRCLE_RADIUS, circle.circle_color);
 	}
 
 }
